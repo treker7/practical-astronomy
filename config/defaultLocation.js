29de62aaa843d5f90011b0ee533f84e2cd84d3f0
Treker Battig <treker.battig@protonmail.com>
2024-05-06 22:54:34 -0400

    Removed uneeded condiguration

--- config/defaultLocation.js
+++
@@ -1,5 +0,0 @@
-﻿var GeographicCoordinate = require('../models/geographicCoorindate');
-
-// Edinboro, PA
-var DEFAULT_LOCATION = new GeographicCoordinate(41.8125, -80.0935);
-module.exports = DEFAULT_LOCATION;
